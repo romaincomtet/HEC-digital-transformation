64a61eebc6feaedf52c11490e3e1c7792dc38726
Romain Comtet <romain.comtet@epitech.eu>
2022-05-30 15:35:13 +0200

    Updates

---
+++ _next/static/cDSdcRI9LkYw5-srAoxo5/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-f56f9d09f5588d89.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
